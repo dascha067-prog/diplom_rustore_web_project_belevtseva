2710dd85674d4c29264b7eff3e36dec50aa35adb
Darya Belevtseva <dascha067@gmail.com>
2026-01-17 21:29:21 +0300

    Написаны тесты на web приложение rustore

--- tests/web_tests/test_home_page.py
+++ tests/web_tests/test_home_page.py
@@ -11,12 +11,13 @@ from tests.pages.home_page import HomePage
 @allure.tag("ui")
 @pytest.mark.ui
 class TestHomePage:
-
+    @allure.title("Открытие главной страницы")
     @allure.story("Проверка открытия главной страницы")
     @allure.severity(allure.severity_level.CRITICAL)
     def test_open_home_page(self):
         HomePage().open().should_be_opened().should_have_logo()
 
+    @allure.title("Проверка наличия кнопки «Найти»")
     @allure.story("Проверка наличия кнопки найти")
     @allure.severity(allure.severity_level.NORMAL)
     def test_search_button_visible(self):
