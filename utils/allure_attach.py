89013432474743028e6ac8b7c1c59244ba9cee3a
Darya Belevtseva <dascha067@gmail.com>
2026-01-31 20:16:34 +0300

    Оформлен диплом

--- utils/allure_attach.py
+++ utils/allure_attach.py
@@ -1,44 +1,3 @@
-# import allure
-# from allure_commons.types import AttachmentType
-# from selene import browser as selene_browser
-#
-# driver = selene_browser.driver
-# png = driver.get_screenshot_as_png()
-#
-#
-# def add_screenshot(driver):
-#     png = driver.get_screenshot_as_png()
-#     allure.attach(
-#         png,
-#         name="screenshot",
-#         attachment_type=AttachmentType.PNG,
-#         extension=".png"
-#     )
-#
-#
-# def add_logs(driver):
-#     log = "".join(f"{text}\n" for text in driver.execute("getLog", {'type': 'browser'})['value'])
-#     allure.attach(log, "browser_logs", AttachmentType.TEXT, ".log")
-#
-#
-# def add_html(driver):
-#     html = driver.page_source
-#     allure.attach(html, "page_source", AttachmentType.HTML, ".html")
-#
-#
-# def add_video(session_id: str, selenoid_base_url: str):
-#     video_url = f"{selenoid_base_url.rstrip('/')}/video/{session_id}.mp4"
-#     html = f"""
-#     <html>
-#       <body>
-#         <p><a href="{video_url}" target="_blank">Open video</a></p>
-#         <video width="100%" height="100%" controls>
-#           <source src="{video_url}" type="video/mp4">
-#         </video>
-#       </body>
-#     </html>
-#     """
-#     allure.attach(html, name="video", attachment_type=allure.attachment_type.HTML)
 import allure
 from allure_commons.types import AttachmentType
 
@@ -69,7 +28,6 @@ def add_html(driver):
 
 
 def add_video(session_id: str, selenoid_host: str):
-    # selenoid_host пример: "selenoid.autotests.cloud"
     video_url = f"https://{selenoid_host}/video/{session_id}.mp4"
     html = f"""
     <html>
